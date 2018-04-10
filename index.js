c4a833bd9fdd9945dd91f835464d30dafb3252a4
Rogério Almeida <rogerioroyalmeida@gmail.com>
2018-04-09 23:17:13 -0300

    Adicionando CORS

--- index.js
+++ index.js
@@ -7,6 +7,11 @@ const mysql = require('mysql');
 //configurando o body parser para pegar POSTS mais tarde
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
 
 //definindo as rotas
 const router = express.Router();
